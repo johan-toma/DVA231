b68fa353f0b8676c028c684e0dab9173f1cb0038
Johan Toma <98798563+johan-toma@users.noreply.github.com>
2023-09-16 14:57:24 +0200

    Create admin.php

---
+++ assignment2 part 1/html/admin.php
@@ -0,0 +1,16 @@
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="../css/admin-style.css"></link>
+        <title>NASA - Admin Page</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        
+        <form action="index.php" method="post"> 
+            <label for="fileInput">Please enter an XML-file to replace the newspieces with!</label>
+            <input id="fileInput" type="text" placeholder="Enter XML File" name="fileInput" required>
+            <input type="submit" value="Apply">
+        </form>
+    </body>
+</html>
